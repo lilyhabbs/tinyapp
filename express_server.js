8b0e13b9cd2a4f0cf19b3c42b048e297fbf82be2
Lily Habberfield <lilyhabbs@gmail.com>
2021-04-13 02:58:40 +0000

    Save shortURL: longURL POST requests and add redirect after form submission

--- express_server.js
+++ express_server.js
@@ -29,8 +29,9 @@ app.get('/urls/:shortURL', (req, res) => {
 });
 
 app.post('/urls', (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send('Ok');         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.listen(PORT, () => {
