eddf30939dc86f9a49ccb2efcb6f76b6863643f1
Lily Habberfield <lilyhabbs@gmail.com>
2021-04-13 01:38:25 +0000

    Add a POST route to receive the form submission

--- express_server.js
+++ express_server.js
@@ -9,6 +9,8 @@ const urlDatabase = {
   '9sm5xK': 'http://www.google.com',
 };
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
@@ -21,6 +23,11 @@ app.get('/urls/new', (req, res) => {
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send('Ok');         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
