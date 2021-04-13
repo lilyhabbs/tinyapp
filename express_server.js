488c52fb7e74eab16fc0b9289c4fe7baf1a9a7bc
Lily Habberfield <lilyhabbs@gmail.com>
2021-04-13 03:16:34 +0000

    Add redirect to longURL

--- express_server.js
+++ express_server.js
@@ -28,6 +28,11 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post('/urls', (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
