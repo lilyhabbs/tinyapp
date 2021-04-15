d36f7525d8ec3b9e718071c1237b787490a87236
Lily Habberfield <lilyhabbs@gmail.com>
2021-04-15 02:41:17 +0000

    Create separate file for helper functions

---
+++ helpers.js
@@ -0,0 +1,27 @@
+const generateRandomString = () => Math.random().toString(36).substr(2, 6);
+
+const getUserByEmail = function(email, users) {
+  for (const user in users) {
+    if (users[user].email === email) {
+      return users[user];
+    }
+  }
+};
+
+const getUrlsForUser = (id, urlDatabase) => {
+  const userUrls = {};
+
+  for (const shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === id) {
+      userUrls[shortURL] = urlDatabase[shortURL];
+    }
+  }
+
+  return userUrls;
+};
+
+module.exports = {
+  generateRandomString,
+  getUserByEmail,
+  getUrlsForUser
+};
